89afb1c0d47905a00f35b9ab4732a636247dee02
Philipp Bartsch <tastybug@tastybug.com>
2021-08-23 23:30:13 +0200

    Simple provisioning of the root user env (dotfiles, git install, some security improvements)

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Prerequisites:
 * have Terraform installed
 
 Run `terraform apply -var hcloud_token=$(cat ~/.hetzner/token)`.
+
+### Further reads on cloud-init
+
+* https://cloudinit.readthedocs.io/en/latest/topics/examples.html
+* https://www.digitalocean.com/community/tutorials/how-to-use-cloud-config-for-your-initial-server-setup
--- main.tf
+++ main.tf
@@ -15,20 +15,40 @@ variable "hcloud_token" {
   sensitive = true
 }
 
+locals {
+  pubkey_content = file("~/.ssh/id_rsa.pub")
+}
+
 # Create a new server running debian
 resource "hcloud_server" "node1" {
   name        = "node1"
   image       = "debian-11"
   server_type = "cx11"
   ssh_keys    = [hcloud_ssh_key.default.id]
+
+  # https://cloudinit.readthedocs.io/en/latest/topics/examples.html
+  user_data   = <<-EOT
+  #cloud-config
+  package_update: true
+  packages:
+   - git
+  users:
+   - name: root
+  runcmd:
+   - [usermod, --shell, /bin/bash, root]
+   - [git, clone, https://github.com/tastybug/dotfiles-ansible, /root/.dotfiles]
+   - [sed, -i, -e, '$aPasswordAuthentication no', /etc/ssh/sshd_config]
+   - [systemctl, restart, sshd]
+  EOT
 }
 
 resource "hcloud_ssh_key" "default" {
   name       = "TF managed ssh key"
-  public_key = file("~/.ssh/id_rsa.pub")
+  public_key = local.pubkey_content 
 }
 
 output "instance_ip" {
   description = "IP of the VM"
   value       = hcloud_server.node1.ipv4_address
 }
+
