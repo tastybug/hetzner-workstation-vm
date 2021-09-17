5fd1dc91b36315a27fb4cf65e6bebdaa058e74e2
Philipp Bartsch <tastybug@tastybug.com>
2021-09-18 00:26:48 +0200

    use ansible for basic VM setup, incl user creation

--- main.tf
+++ main.tf
@@ -16,7 +16,8 @@ variable "hcloud_token" {
 }
 
 locals {
-  pubkey_content = file("~/.ssh/id_rsa.pub")
+  privkey_location = "~/.ssh/ansible"
+  pubkey_location = "~/.ssh/ansible.pub"
 }
 
 # Create a new server running debian
@@ -26,25 +27,28 @@ resource "hcloud_server" "node1" {
   server_type = "cx11"
   ssh_keys    = [hcloud_ssh_key.default.id]
 
-  # https://cloudinit.readthedocs.io/en/latest/topics/examples.html
-  user_data   = <<-EOT
-  #cloud-config
-  package_update: true
-  packages:
-   - git
-  users:
-   - name: root
-  runcmd:
-   - [usermod, --shell, /bin/bash, root]
-   - [git, clone, https://github.com/tastybug/dotfiles-ansible, /root/.dotfiles]
-   - [sed, -i, -e, '$aPasswordAuthentication no', /etc/ssh/sshd_config]
-   - [systemctl, restart, sshd]
-  EOT
+  ## hack: this remote-exec provisioner will retry until a ssh connection can be made (ergo: the instance is up)
+  ## at this point the local-exec below can start. The local exec has no retry and no idea when the instance is ready.
+  provisioner "remote-exec" {
+    inline = ["echo instance is up!"]
+
+    connection {
+      host        = self.ipv4_address
+      type        = "ssh"
+      user        = "root"
+      private_key = file(local.privkey_location)
+    }
+  }
+
+  provisioner "local-exec" {
+	command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.ipv4_address},' --private-key ${local.privkey_location} -e 'pubkey_location=${local.pubkey_location}' ansible-setup.yml"
+  }
+
 }
 
 resource "hcloud_ssh_key" "default" {
   name       = "TF managed ssh key"
-  public_key = local.pubkey_content 
+  public_key = file(local.pubkey_location)
 }
 
 output "instance_ip" {
